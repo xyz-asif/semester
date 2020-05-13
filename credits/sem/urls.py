176c7d0fc8d8bc11fc32a8865c55ef9a4a155c1c
Bernard Namangala <bernardnamangala@gmail.com>
2020-05-13 16:37:16 +0200

    commit

--- credits/sem/urls.py
+++ credits/sem/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 app_name = 'sem'
+
 urlpatterns = [
     path("", views.home, name="home"),
-    path("<str:pk>/",views.sem, name="sem"),
-    path("/semester",views.sem , name="sem"),
-    path("/subject",views.sub,name="sub"),
-    path("/lecture",views.lecture,name="lecture")
+    path("<int:pk>/",views.sem, name="sem"),
+    path("semester/",views.sem , name="sem"),
+    path("subject/<int:pk>/",views.sub,name="sub"),
+    path("lecture/",views.lecture,name="lecture")
 ]
